34563aa0185dd59442bed86b5ad59260a2e8134a
SunGodApollo <apple.ortiz@hotmail.com>
2023-05-10 00:04:42 -0700

    finished frontend preparedness assessment

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
@@ -7,15 +6,10 @@ const App = () => {
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
-      <div className="form">
-        <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
-        </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
+
+      
         <ModalComponent />
-      </div>
+
     </div>
   )
 }
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,91 @@
-import React from "react"
+import React, { useState } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Input,
+  Label,
+  Form,
+  FormGroup,
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+import "../App.css"
 
-const ModalComponent = () => {
-  return <></>
+
+
+
+const ModalComponent = (props) => {
+
+  const { className } = props
+  const [modal, setModal] = useState(false)
+
+
+  const [inputValue, setInputValue] = useState('')
+  
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+
+  const toggle = () => setModal(!modal)
+
+  const handleResetClick = () => {
+    setInputValue('')
+  }
+
+
+
+
+  return (
+
+    <div className="container">
+    <Form inline onSubmit={(e) => e.preventDefault()} className="form">
+      <FormGroup>
+        <Label for="name" className="label">Enter your name</Label>
+        <Input 
+          type="text" 
+          value={inputValue} 
+          onChange={handleInputChange} 
+        >
+        </Input>
+      </FormGroup>
+
+
+      <Button onClick={toggle}>
+        Click Me
+      </Button>
+      <Button onClick={handleResetClick}>
+        Reset
+      </Button>
+    </Form>
+    <Modal
+      isOpen={modal}
+      toggle={toggle}
+      className={className}
+
+    >
+      <ModalHeader toggle={toggle}>Hello there {inputValue}!</ModalHeader>
+      <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </ModalBody>
+
+    </Modal>
+  </div>
+);
 }
+
+ModalComponent.propTypes = {
+className: PropTypes.string,
+};
+  
+
 
 export default ModalComponent
